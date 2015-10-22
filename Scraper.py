71ccece85a47aa56c0334d00d5d462e7ba91862f
Dan <dbbondy@googlemail.com>
2015-10-21 17:51:34 -0700

    Initial Commit of files to Git Repo

---
+++ Scraper.py
@@ -0,0 +1,22 @@
+import praw
+from pprint import pprint
+import Email
+
+__author__ = 'Dan'
+
+r = praw.Reddit(user_agent='Testing reddit API for Dan B.')
+
+submissions = r.get_subreddit('starcraft').get_hot(limit=10)
+
+links = {}
+for submission in submissions:
+    links[str(submission)] = str(submission.url)
+
+pprint(links)
+
+
+##TODO: now build up email and send it out.
+
+
+
+
